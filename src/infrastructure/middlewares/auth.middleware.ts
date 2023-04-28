9166c0b1c5bd8184a11b8a5568a3f9b5de693d5b
Carlos Coral <coralcarlos21@gmail.com>
2023-04-27 22:07:13 -0500

    Authentication and authorization

---
+++ src/infrastructure/middlewares/auth.middleware.ts
@@ -0,0 +1,36 @@
+import * as jwt from 'jsonwebtoken';
+import { Logger } from '../../domain/models/logger';
+import { Application, NextFunction, Request, Response } from 'express';
+import { Mountable } from '../interfaces/mountable';
+import { Locals } from '../adapters/locals';
+
+export class AuthMiddleware extends Mountable<Application> {
+  constructor(private logger: Logger) {
+    super();
+  }
+
+  static verifyAuth(req: Request, res: Response, next: NextFunction) {
+    const { jwtSecret } = Locals.getConfig();
+    const { authorization } = req.headers;
+    if (authorization) {
+      const [type, token] = authorization.split(' ');
+      if (type === 'Bearer' && token) {
+        try {
+          jwt.verify(token, jwtSecret);
+          return next();
+        } catch (e) {
+          return res.status(401).json({ error: 'Not Authorized' });
+        }
+      }
+    }
+    return res.status(401).json({ error: 'Not Authorized' });
+  }
+
+  mount(app: Application): Application {
+    this.logger.info('Mounting AuthMiddleware');
+    app = app.use((req: Request, res: Response, next: NextFunction) =>
+      AuthMiddleware.verifyAuth(req, res, next),
+    );
+    return app;
+  }
+}
--- src/infrastructure/routes/invoice.routes.ts
+++ src/infrastructure/routes/invoice.routes.ts
@@ -2,6 +2,7 @@ import { Application, Router } from 'express';
 import { Routes } from './routes';
 import { Logger } from '../../domain/models/logger';
 import { InvoiceController } from '../controllers.ts/invoice.controller';
+import { AuthMiddleware } from '../middlewares/auth.middleware';
 
 export class InvoiceRoutes extends Routes {
   constructor(private invoiceController: InvoiceController, logger: Logger) {
@@ -13,11 +14,11 @@ export class InvoiceRoutes extends Routes {
 
     const router = Router();
 
-    router.get('', (req, res, next) => this.invoiceController.getInvoices(req, res, next));
-    router.get('/:id', (req, res, next) => this.invoiceController.getInvoice(req, res, next));
-    router.post('', (req, res, next) => this.invoiceController.createInvoice(req, res, next));
-    router.put('/:id', (req, res, next) => this.invoiceController.updateInvoice(req, res, next));
-    router.delete('/:id', (req, res, next) => this.invoiceController.deleteInvoice(req, res, next));
+    router.get('', AuthMiddleware.verifyAuth, (req, res, next) => this.invoiceController.getInvoices(req, res, next));
+    router.get('/:id', AuthMiddleware.verifyAuth, (req, res, next) => this.invoiceController.getInvoice(req, res, next));
+    router.post('', AuthMiddleware.verifyAuth, (req, res, next) => this.invoiceController.createInvoice(req, res, next));
+    router.put('/:id', AuthMiddleware.verifyAuth, (req, res, next) => this.invoiceController.updateInvoice(req, res, next));
+    router.delete('/:id', AuthMiddleware.verifyAuth, (req, res, next) => this.invoiceController.deleteInvoice(req, res, next));
 
     app.use('/invoice', router);
     return app;
